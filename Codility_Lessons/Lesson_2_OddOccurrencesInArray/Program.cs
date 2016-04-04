b6516995b2d31134a541b2c781644e75722520da
Hector Eduardo Aguirre Santiago <haguirres@intellego.com.mx>
2016-04-04 17:40:30 -0500

    OddOcurrencesInArray Finished

---
+++ Codility_Lessons/Lesson_2_OddOccurrencesInArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson_2_OddOccurrencesInArray
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] A = { 9, 4, 9, 3, 9, 3, 9, 7, 7 };
+            Console.WriteLine("[{0}]", string.Join(", ", A));
+            int value = GetUnpairedValue(A);
+            Console.WriteLine("Valor sin par: " + value);
+        }
+
+        private static int GetUnpairedValue(int[] A)
+        {
+            //return A.Aggregate((x, y) => x ^ y);
+
+            int current = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                current = current ^ A[i];
+                Console.WriteLine(current);
+            }
+            return current;
+        }
+    }
+}
