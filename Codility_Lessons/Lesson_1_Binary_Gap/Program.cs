81c738c53d633dd141ddd0df3a21a22638635f17
Hector Eduardo Aguirre Santiago <haguirres@intellego.com.mx>
2016-04-04 12:23:40 -0500

    BinaryGap Finished

--- Codility_Lessons/Lesson_1_Binary_Gap/Program.cs
+++ Codility_Lessons/Lesson_1_Binary_Gap/Program.cs
@@ -10,6 +10,52 @@ namespace Lesson_1_Binary_Gap
     {
         static void Main(string[] args)
         {
+            int N = 529;
+            string binaryNumber = Convert.ToString(N, 2);
+            Console.WriteLine("Binary Number:"+ binaryNumber);
+            int binaryGap = GetBinaryGap(binaryNumber);
+            Console.WriteLine("Binary Gap: "+binaryGap);
+        }
+
+        private static int GetBinaryGap(string binaryNumber)
+        {
+            char[] binary = binaryNumber.ToCharArray();
+            int firstone = 0;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (binary[i] == '1')
+                {
+                    firstone = i;
+                    break;
+                }
+            }
+            int lastone = 0;
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                if (binary[i] == '1')
+                {
+                    lastone = i;
+                    break;
+                }
+            }
+            int count = 0;
+            int max = 0;
+            for (int i = firstone; i <= lastone; i++)
+            {
+                if (binary[i] == '0')
+                {
+                    count++;
+                }
+                else
+                {
+                    if (max < count)
+                    {
+                        max = count;
+                    }
+                    count = 0;
+                }
+            }
+            return max;
         }
     }
 }
