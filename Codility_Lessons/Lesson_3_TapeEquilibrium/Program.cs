cb95ae0c999e1e974652455d0fcc47a7c15a2a78
Hector Eduardo Aguirre Santiago <haguirres@intellego.com.mx>
2016-04-06 13:55:37 -0500

    TapeEquilibrium Finished At 66% (100% Correctness,33% Performance)

---
+++ Codility_Lessons/Lesson_3_TapeEquilibrium/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson_3_TapeEquilibrium
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] A = new int[] { 3, 1, 2, 4, 3 };
+
+            Console.WriteLine(Solution(A));
+        }
+
+        private static int Solution(int[] A)
+        {
+            int sumN = 0;
+            int[] array = new int[A.Length - 1];
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                int sumP = 0;
+                sumN += A[i];
+
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    sumP += A[j];
+                }
+                array[i] = Math.Abs(sumN - sumP);
+            }
+            return array.Min();
+        }
+    }
+}
