ad81eb07133f92d92a77e778c8a540726ac90ea4
Hector Eduardo Aguirre Santiago <haguirres@intellego.com.mx>
2016-04-06 15:06:47 -0500

    PermMissingElem Finished 100%

---
+++ Codility_Lessons/Lesson_3_PermMissingElem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson_3_PermMissingElem
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] A = { 2, 3, 4, 5, 1 };
+            Console.WriteLine(MissingElement(A));
+        }
+
+        private static int MissingElement(int[] A)
+        {
+            int element = 0;
+            if (A != null)
+            {
+                if (A.Length > 0)
+                {
+                    var B = A.OrderBy(s => Convert.ToInt32(s)).ToArray();
+                    for (int i = 0; i < A.Length; i++)
+                    {
+                        if (B[i] != i + 1)
+                        {
+                            element = i + 1;
+                            break;
+                        }
+                    }
+                    element = element == 0 ? A.Length + 1 : element;
+                }
+                else
+                {
+                    element += 1;
+                }
+            }
+            return element;
+        }
+    }
+}
