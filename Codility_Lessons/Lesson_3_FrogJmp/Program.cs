3e86d1e3986f521de3cf893d31e08a23e2529d8b
Hector Eduardo Aguirre Santiago <haguirres@intellego.com.mx>
2016-04-06 12:13:54 -0500

    Frog Jumps Completed!

---
+++ Codility_Lessons/Lesson_3_FrogJmp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson_3_FrogJmp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 85;
+            decimal d = 30;
+            GetJumpsNumber(x, y, d);
+        }
+
+        private static void GetJumpsNumber(int x, int y, decimal d)
+        {
+            int jumps = 0;
+
+            if (x <= y)
+            {
+                if (x < y)
+                {
+                    jumps = (int)Math.Ceiling((y - x) / d);
+                }
+            }
+
+            Console.WriteLine("Num. Jumps: " + jumps);
+        }
+    }
+}
